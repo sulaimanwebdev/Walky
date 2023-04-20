8e2b27a5b28e84969b1f05881fcb2f11a30f851e
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-20 21:47:13 +0500

    errors fixed

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -120,8 +120,8 @@ const Slider = () => {
             );
           })}
 
-      <div ref={navigationPrevRef} className='whiteGradientLeft z-30 absolute top-1/2 left-0 -translate-y-1/2 flex items-center justify-start w-[80px] h-full cursor-pointer'><svg width="20" height="30" viewBox="0 0 31 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M27 42L4.00003 23L27 4" stroke="#4683B6" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
-      <div ref={navigationNextRef} className='whiteGradientRight z-30 absolute top-1/2 right-0 -translate-y-1/2 flex items-center justify-end w-[80px] h-full cursor-pointer'><svg width="20" height="30" viewBox="0 0 31 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 4L27 23L4 42" stroke="#4683B6" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
+      <div ref={navigationPrevRef} className='whiteGradientLeft z-30 absolute top-1/2 left-0 -translate-y-1/2 flex items-center justify-start w-[80px] h-full cursor-pointer'><svg width="20" height="30" viewBox="0 0 31 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M27 42L4.00003 23L27 4" stroke="#4683B6" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/></svg></div>
+      <div ref={navigationNextRef} className='whiteGradientRight z-30 absolute top-1/2 right-0 -translate-y-1/2 flex items-center justify-end w-[80px] h-full cursor-pointer'><svg width="20" height="30" viewBox="0 0 31 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 4L27 23L4 42" stroke="#4683B6" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/></svg></div>
 
 
         </Swiper>
