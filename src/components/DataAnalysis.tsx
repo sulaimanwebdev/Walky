ecbeb5dfce0fdbb08f13ae458a940c6158444550
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-20 12:57:36 +0500

    analysis added

---
+++ src/components/DataAnalysis.tsx
@@ -0,0 +1,110 @@
+const dataAnalysis = () => {
+
+    let data = [
+        {
+          name: "AFIB",
+          dataSets: 53,
+          accuracyDataSets: 93,
+        },
+        {
+          name: "Normal",
+          dataSets: 14,
+          accuracyDataSets: 94,
+        },
+        {
+          name: "SB",
+          dataSets: 67,
+          accuracyDataSets: 97,
+        },
+        {
+          name: "VT",
+          dataSets: 72,
+          accuracyDataSets: 92,
+        },
+        {
+          name: "PVC",
+          dataSets: 65,
+          accuracyDataSets: 95,
+        },
+        {
+          name: "VF",
+          dataSets: 65,
+          accuracyDataSets: 95,
+        },
+        {
+          name: "SA",
+          dataSets: 70,
+          accuracyDataSets: 90,
+        },
+        {
+          name: "SVT",
+          dataSets: 53,
+          accuracyDataSets: 93,
+        },
+      ];
+
+
+    return (
+      <>
+        <div className="customShadow bg-white p-5 rounded-xl">
+          <div className="font-bold text-[25px] text-[#777777] mb-5">
+            Data Analysis
+          </div>
+  
+          <div className="grid grid-cols-[120px,1fr,1fr] gap-10">
+            <div>
+              <div className="font-[600] text-[18px] mb-4">Name</div>
+              <div className="flex flex-col gap-5">
+                {data.map((item) => (
+                  <div key={Math.random()}>{item.name}</div>
+                ))}
+              </div>
+            </div>
+  
+            <div>
+              <div className="font-[600] text-[18px] mb-4">% of Data Sets</div>
+              <div className="flex flex-col gap-5">
+                {data.map((item) => (
+                  <div className="flex items-center gap-3 w-full" key={Math.random()}>
+                    <div style={{ fontSize: "17px", color: "#777777" }}>
+                      {item.dataSets}%
+                    </div>
+                    <div className="flex rounded-full bg-yale bg-opacity-30 w-full h-[15px] overflow-hidden">
+                      <div
+                        className={`bg-walkkyBlue h-full rounded-full`}
+                        style={{ width: `${item.dataSets}%`}}
+                      ></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+  
+            <div>
+              <div className="font-[600] text-[18px] mb-4">
+                Accuracy across all data sets
+              </div>
+              <div className="flex flex-col gap-5">
+                {data.map((item) => (
+                  <div className="flex items-center gap-3 w-full" key={Math.random()}>
+                    <div style={{ fontSize: "17px", color: "#777777" }}>
+                      {item.accuracyDataSets}%
+                    </div>
+                    <div className="flex rounded-full bg-lightTeal bg-opacity-30 w-full h-[15px] overflow-hidden">
+                      <div
+                        className={`bg-lightTeal h-full rounded-full`}
+                        style={{ width: `${item.accuracyDataSets}%`}}
+                      ></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default dataAnalysis;
+  
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,27 @@
 
-import React, { useState } from "react";
 import { ContentContainer } from "../components/ContentContainer";
 import InsightWidget from "../components/InsightWidget";
+import DataAnalysis from "../components/DataAnalysis";
 
 const Home = () => {
   return (
     <>
       <ContentContainer>
         <div className="font-bold text-[28px] mb-5">Walkky Data Analysis</div>
-        <div className="grid grid-cols-3 gap-5 w-full">
+
+        <div className="grid grid-cols-3 gap-7 w-full mb-8">
           <InsightWidget title="Total Accuracy" value="98%" icon="/assets/i-icon.svg" bg="lightTeal"/>
-          <InsightWidget title="Total Accuracy" value="98%" icon="/assets/i-icon.svg" bg="walkkyBlue"/>
+          <InsightWidget title="Single Beat Accuracy" value="96%" icon="/assets/i-icon.svg" bg="walkkyBlue"/>
         </div>
+ 
+        <DataAnalysis/>
+
+        <div className="customShadow bg-white p-5 rounded-xl mt-8">
+          <div className="font-bold text-[25px] text-[#777777] mb-1">Data Sets</div>
+          <div className="mb-5">Most recent</div>
+
+        </div>
+
       </ContentContainer>
     </>
   );
